1655aa7405a8da7841f3e95ed41ae96b461dce3b
LimYunhwi <88yhtserof@naver.com>
2021-12-24 20:22:03 +0900

    #3 Main Page : 메인 화면 레이아웃 (2)
    1. 원형 타이머 레이아웃 배치
    2. Navigation Bar 지우기
    3. UIImageView 배치
    4. 그외 여러 View 재배치

--- ProjectTimeToGo/MainViewController.swift
+++ ProjectTimeToGo/MainViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 class MainViewController: UIViewController {
 
     var goal: Goal?
-    
+    let shap = CAShapeLayer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        addLayerCircleTimer()
+    }
+    
+    func addLayerCircleTimer() {
+        let circlePath = UIBezierPath(arcCenter: view.center,
+                                      radius: 150,
+                                      startAngle: -(.pi/2),
+                                      endAngle: .pi * 2,
+                                      clockwise: true)
+        
+        let trackCircle = CAShapeLayer()
+        trackCircle.fillColor = UIColor.clear.cgColor
+        trackCircle.path = circlePath.cgPath
+        trackCircle.lineWidth = 30
+        trackCircle.strokeColor = UIColor.systemGray2.cgColor
+        
+        shap.path = circlePath.cgPath
+        shap.lineWidth = 15
+        shap.strokeColor = UIColor.black.cgColor
+        shap.fillColor = UIColor.clear.cgColor
+        
+        view.layer.addSublayer(trackCircle)
+        view.layer.addSublayer(shap)
     }
 }
