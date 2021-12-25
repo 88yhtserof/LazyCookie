7f7ff9f3d9bf35d25d5e0e90cedacff0e069f1af
LimYunhwi <88yhtserof@naver.com>
2021-12-25 21:27:05 +0900

    #3 Main Page : 메인 화면 레이아웃 (4)

--- ProjectTimeToGo/MainViewController.swift
+++ ProjectTimeToGo/MainViewController.swift
@@ -8,33 +8,38 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     var goal: Goal?
     let shap = CAShapeLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        self.navigationItem.hidesBackButton = true
+        
+        let imageCookie : UIImageView = UIImageView(frame: CGRect(x: view.frame.midX, y: view.frame.height-550, width: 100, height: 100))
+        imageCookie.image = UIImage(named: "firstpage.png")
+        
         addLayerCircleTimer()
+        view.addSubview(imageCookie)
     }
     
     func addLayerCircleTimer() {
-        let circlePath = UIBezierPath(arcCenter: view.center,
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: view.frame.width/2, y: view.frame.height-550),
                                       radius: 130,
                                       startAngle: -(.pi/2),
                                       endAngle: .pi * 2,
                                       clockwise: true)
         
         let trackCircle = CAShapeLayer()
-        trackCircle.fillColor = UIColor.clear.cgColor
+        trackCircle.fillColor = UIColor(displayP3Red: 252/255, green: 251/255, blue: 238/255, alpha: 1).cgColor
         trackCircle.path = circlePath.cgPath
         trackCircle.lineWidth = 40
         trackCircle.strokeColor = UIColor(displayP3Red: 252/255, green: 251/255, blue: 238/255, alpha: 1).cgColor
         
         shap.path = circlePath.cgPath
         shap.lineWidth = 20
-        shap.strokeColor = UIColor(displayP3Red: 239/255, green: 155/255, blue: 131/255, alpha: 1).cgColor
+        shap.strokeColor = UIColor(displayP3Red: 243/255, green: 220/255, blue: 152/255, alpha: 1).cgColor
         shap.fillColor = UIColor.clear.cgColor
         
         view.layer.addSublayer(trackCircle)
