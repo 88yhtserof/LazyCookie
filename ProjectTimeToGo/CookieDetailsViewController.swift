fd7e1d84f029e7392acb5972ef53e9f68f89692b
LimYunhwi <88yhtserof@naver.com>
2022-01-17 21:25:38 +0900

    #3 Main Page : 쿠키 상세설명 (2)
    1. 쿠키관련 개발자 설명란 추가
    2. 쿠키 이름, 설명, 개발자 설명 작성

--- ProjectTimeToGo/CookieDetailsViewController.swift
+++ ProjectTimeToGo/CookieDetailsViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class CookieDetailsViewController: UIViewController {
 
+    @IBOutlet weak var lblCookieName: UILabel!
+    @IBOutlet weak var lblCookieDetails: UILabel!
+    @IBOutlet weak var lblInfo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.lblCookieName.text = "초코칩 쿠키"
+        self.lblCookieDetails.text =
+            """
+            1930년 미국의 어느 작은 식당의 재료 부족으로 만들어진 초코칩 쿠키!
+            잘게 조각한 초콜릿이 잔뜩 들어가 달콤하며, 바삭한 것부터 꾸덕한 것까지 질감이 다양하다.
+            """
+        self.lblInfo.text = """
+            개발자는 바삭한 초코칩 쿠키를 좋아해요:)
+            """
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
