6a73c707f9ba1e0a74c06b738a04be149926e0c9
LimYunhwi <88yhtserof@naver.com>
2022-01-28 15:01:55 +0900

    #4 Result page : 결과 화면 레이아웃 (3)
    1. 결과 화면 삭제
    2. 시간 달성 시 결과 전달 Alert 뜨도록 수정
    3. '나가기' 버튼 클릭 시 뜨는 Alert 수정
    4. '나가기' 버튼의 '네' 버튼 클릭 시 결과 Alert 뜨도록 수정

--- ProjectTimeToGo/MainViewController.swift
+++ ProjectTimeToGo/MainViewController.swift
@@ -75,10 +75,13 @@ class MainViewController: UIViewController {
         let alert = UIAlertController(title: "나가기", message: "홈 화면으로 되돌아가시겠습니까?", preferredStyle: UIAlertController.Style.alert)
         let actionYes = UIAlertAction(title: "네", style: UIAlertAction.Style.default) {
             _ in
-            self.navigationController?.popToRootViewController(animated: true)
+            let message = "넌 큰일났다."
+            self.presentResultAlert(message: message)
         }
         let actionNo = UIAlertAction(title: "아니오", style: UIAlertAction.Style.default, handler: nil)
         
+        actionYes.setValue(UIColor(displayP3Red: 51/255, green: 36/255, blue: 18/255, alpha: 1), forKey: "TitleTextColor")
+        actionNo.setValue(UIColor(displayP3Red: 51/255, green: 36/255, blue: 18/255, alpha: 1), forKey: "TitleTextColor")
         alert.addAction(actionYes)
         alert.addAction(actionNo)
         
@@ -108,16 +111,8 @@ class MainViewController: UIViewController {
         self.count += 1
         
         if self.count >= Int(self.duration) {
-            let alert = UIAlertController(title: "목표 달성", message: "목표 시간을 달성했습니다.", preferredStyle: .alert)
-            let action = UIAlertAction(title: "쿠키 획득", style: .default){_ in
-                guard let ResultViewController = self.storyboard?.instantiateViewController(identifier: "ResultViewController") as? ResultViewController else{ return }
-                
-                self.navigationController?.pushViewController(ResultViewController, animated: true)
-            }
-            action.setValue(UIColor.brown, forKey: "titleTextColor")
-            alert.addAction(action)
-            
-            present(alert, animated: true, completion: nil)
+            let message = "오늘도 수고했어!"
+            self.presentResultAlert(message: message)
             
             self.timer.invalidate()
         }
@@ -140,5 +135,22 @@ class MainViewController: UIViewController {
         guard let CookieDetailsViewController = self.storyboard?.instantiateViewController(identifier: "CookieDetailsViewController") as? CookieDetailsViewController else {return}
         
         self.present(CookieDetailsViewController, animated: true, completion: nil)
+    }
+    
+    func presentResultAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        let attributedString = NSMutableAttributedString(string: message)
+        let font = UIFont(name: "NanumBarunpenOTF", size: 16)
+        attributedString.addAttribute(.font, value: font!, range: (message as NSString).range(of: "\(message)"))
+        alert.setValue(attributedString, forKey: "attributedMessage")
+        
+        let action = UIAlertAction(title: "HOME", style: .default){
+            _ in
+            self.navigationController?.popToRootViewController(animated: true)
+        }
+        action.setValue(UIColor.brown, forKey: "titleTextColor")
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
